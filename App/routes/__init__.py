cae229ea2559ab5ee93112f81b94126b4aaddea7
Maxi <eulamaxi@gmail.com>
2025-09-16 12:20:38 -0300

    testeado con roles (fuera de tests)

--- App/routes/__init__.py
+++ App/routes/__init__.py
@@ -1,4 +1,5 @@
 from .administrador_resource import Administrador
+from .auth_resource import Auth
 from .fecha_resource import Fecha
 from .persona_resource import Persona
 from .reserva_resource import Reserva
